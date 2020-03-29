5e1463357177f02bf28c651d30bfd6700d0d3768
PhalugunaRao <phalu900@gmail.com>
2020-03-29 14:25:09 +0530

    insert and fetch data from db

--- app/src/main/java/com/ctrl/demolivedata/room/BookDao.kt
+++ app/src/main/java/com/ctrl/demolivedata/room/BookDao.kt
@@ -1,4 +1,18 @@
 package com.ctrl.demolivedata.room
 
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+
+@Dao
 interface BookDao {
+    @Insert
+    fun insert(book :Book)
+
+    /*@Query("SELECT * FROM books")
+    fun getAllBooks():List<Book>
+*/
+    @get:Query("SELECT * FROM books")
+    val allBooks: LiveData<List<Book>>
 }
--- app/src/main/java/com/ctrl/demolivedata/room/BookRoomDatabase.kt
+++ app/src/main/java/com/ctrl/demolivedata/room/BookRoomDatabase.kt
@@ -1,4 +1,36 @@
 package com.ctrl.demolivedata.room
 
-class BookRoomDatabase {
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [Book::class], version = 1)
+abstract class BookRoomDatabase: RoomDatabase() {
+
+    abstract  fun bookDao(): BookDao
+
+    companion object{
+        private  var bookRoomInstance:  BookRoomDatabase?=null
+
+        fun getDatabase(context: Context): BookRoomDatabase? {
+            if (bookRoomInstance == null) {
+
+                synchronized(BookRoomDatabase::class.java) {
+                    if (bookRoomInstance == null) {
+                        bookRoomInstance = Room.databaseBuilder<BookRoomDatabase>(context.applicationContext,
+                            BookRoomDatabase::class.java, "book_database")
+                            .build()
+                    }
+                }
+            }
+            return bookRoomInstance
+        }
+
+
+
+
+
+
+    }
 }
